29e816ffe4cb93df2ec633ff925e2aef3fec9d86
vj-web <vijayasekard.web@gmail.com>
2017-07-02 14:41:44 -0400

    http module explained

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 import https from 'https';
-import config,{nodeEnv, logStars} from './config';
+import http from 'http';
 
-console.log(config, nodeEnv)
-logStars("I will achieve my dream")
+https.get('https://www.lynda.com',res=>{
+  console.log("Response status code is " + res.statusCode)
+
+  res.on('data', chunk=>{
+ //   console.log(chunk.toString())
+  })
+})
+console.log('***Server Starts***')
+const server = http.createServer();
+server.listen(8080);
+
+server.on('request',(req,res) =>{
+  res.write('Hello HTTP!\n');
+  setTimeout(() => {
+    res.write('I can do it :)!\n');
+    res.end();  
+  },3000);
+})
